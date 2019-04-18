241c5d309653cbea1f410f2607f7587f65124dba
Michael Hottman <hottmanmichael@gmail.com>
2019-04-18 11:03:04 -0600

    Add try catch to calling reducer handler so that errors don't get suppressed

--- src/utils/create_reducer.js
+++ src/utils/create_reducer.js
@@ -1,17 +1,30 @@
 import isFunction from './is_function';
 
+const callHandler = (handlers, state, action) => name => {
+  try {
+    const handler = handlers[name];
+    return handler(state, action);
+  } catch (e) {
+    console.warn(
+      `An error occurred while calling a reducer called: ${name}. The error stack trace is below.`
+    );
+    console.error(e);
+  } finally {
+    return state;
+  }
+};
+
+const validateHandler = handlers => (name, action) =>
+  handlers.hasOwnProperty(action[name]) && isFunction(handlers[action[name]]);
+
 export default function createReducer(initialState, handlers) {
   return function reducer(state = initialState, action) {
-    if (
-      handlers.hasOwnProperty(action._actionType) &&
-      isFunction(handlers[action._actionType])
-    ) {
-      return handlers[action._actionType](state, action);
-    } else if (
-      handlers.hasOwnProperty(action.type) &&
-      isFunction(handlers[action.type])
-    ) {
-      return handlers[action.type](state, action);
+    const doValidateHandler = validateHandler(handlers, action);
+    const doCallHandler = callHandler(handlers, state, action);
+    if (doValidateHandler(action._actionType)) {
+      return doCallHandler(action._actionType);
+    } else if (doValidateHandler(action.type)) {
+      return doCallHandler(action.type);
     } else {
       return state;
     }
