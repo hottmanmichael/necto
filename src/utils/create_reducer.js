cfa2b64fef6eb1f488a907973039fffc25a9e63f
Michael Hottman <hottmanmichael@gmail.com>
2019-04-18 09:35:58 -0600

    Make sure reducer gets called

--- src/utils/create_reducer.js
+++ src/utils/create_reducer.js
@@ -3,7 +3,7 @@ import isFunction from './is_function';
 export default function createReducer(initialState, handlers) {
   return function reducer(state = initialState, action) {
     if (
-      handlers.hasOwnProperty(action.mockType) &&
+      handlers.hasOwnProperty(action._actionType) &&
       isFunction(handlers[action._actionType])
     ) {
       return handlers[action._actionType](state, action);
