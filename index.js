ddd733ee12f2f7394285543fc95a68622b64094f
Michael Hottman <hottmanmichael@gmail.com>
2019-04-18 16:17:11 -0600

    Allow access to a few helper files

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import Necto from './src/necto';
 export default Necto;
 
-// export createFlow from './src/utils/create_flow';
-// export getInitialFlows from './src/utils/create_flow/get_initial_flows';
-// export ensureRequiredParams from './src/utils/ensure_required_params';
+export createFlow from './src/utils/create_flow';
+export ensureRequiredParams from './src/utils/ensure_required_params';
